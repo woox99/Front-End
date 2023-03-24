68fd638d44c8ac860915f8541ba280c8435f8541
Garett.Janulewicz <janulewiczgarett@gmail.com>
2023-03-24 09:57:08 -1000

    added to arrays reference

--- 03 JS/40 Arrays/script.js
+++ 03 JS/40 Arrays/script.js
@@ -18,3 +18,12 @@ console.log(user);
 // IMPORTANT
 user.length //is the length of the array
 console.log(user.length);
+
+
+// sum numbers in array
+arr = [2, 4, 5, 1, 2];
+var sum=0;
+for( var i=0; i<arr.length; i++){
+    sum += arr[i];
+}
+console.log(sum);
