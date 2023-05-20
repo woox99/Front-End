35ea624cc793b375730afd8d0e05e14accd04716
Garett.Janulewicz <janulewiczgarett@gmail.com>
2023-05-20 08:22:32 -1000

    added to references

--- 20_Trix/40_Shifting_Text_Effect/script.js
+++ 20_Trix/40_Shifting_Text_Effect/script.js
@@ -1,4 +1,15 @@
+/* -- Glow effect -- */
 
+const blob = document.getElementById("blob");
+
+window.onpointermove = event => { 
+  const { clientX, clientY } = event;
+  
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 3000, fill: "forwards" });
+}
 
 /* -- Text effect -- */
 
@@ -7,27 +18,27 @@ const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let interval = null;
 
 const screen = document.querySelector(".screen"),
-    name = document.querySelector(".name");
+      name = document.querySelector(".name");
 
 screen.onmouseenter = event => {  
-    let iteration = 0;
-
-    clearInterval(interval);
-
-    interval = setInterval(() => {
-        name.innerText = name.innerText
-        .split("")
-        .map((letter, index) => {
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    name.innerText = name.innerText
+      .split("")
+      .map((letter, index) => {
         if(index < iteration) {
-            return name.dataset.value[index];
+          return name.dataset.value[index];
         }
-
+      
         return letters[Math.floor(Math.random() * 26)]
-    })
-    .join("");
+      })
+      .join("");
     
     if(iteration >= name.dataset.value.length){ 
-        clearInterval(interval);
+      clearInterval(interval);
     }
     
     iteration += 1 / 3;
